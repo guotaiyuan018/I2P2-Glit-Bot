8be77438263cf10516e4acaf4f42e045a6757e2d
k0c4de <148344642+k0c4de@users.noreply.github.com>
2024-01-01 15:23:57 +0800

    Update game_scene.cpp

--- scene_classes/game_scene.cpp
+++ scene_classes/game_scene.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 Game_scene::Game_scene(){
-    game_interface[PAUSE_BUTTON] = {1075, 20, 1145, 75};
+    game_interface[PAUSE_BUTTON] = {1080, 50, 1150, 150};
 }
 
 void Game_scene::load_scene(){
@@ -17,13 +17,48 @@ void Game_scene::load_scene(){
         if(img) maps.push_back(img);
     }
 
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 2; j++){
+            sprintf(buffer, "./Scenes/battle/controller/%d_%d.png", i, j);
+            ALLEGRO_BITMAP *img = al_load_bitmap(buffer);
+            if(img) controller[i][j] = img;
+        }
+        cur_controller[i] = controller[i][0];
+    }
+    //load hp bar
+    hp_bar.push_back(al_load_bitmap("./Scenes/battle/HP/10.png"));
+    //load bot info
+    bot_info.push_back(al_load_bitmap("./Scenes/battle/bot_info/bullet.png"));
+
     pause[0] = al_load_bitmap("./Scenes/battle/pause/0.png");
     pause[1] = al_load_bitmap("./Scenes/battle/pause/1.png");
 }
 
-void Game_scene::draw_scene(){
+void Game_scene::draw_background(){
     al_draw_bitmap(maps[0], 0, 0, 0);
+}
+
+void Game_scene::draw_ui(){
     al_draw_bitmap(pause[cur_menu], 0, 0, 0);
+    for(int i = 0; i < 4; i++){
+        al_draw_bitmap(cur_controller[i], 0, 0, 0);
+    }
+    al_draw_bitmap(hp_bar[0], 0, 0, 0);
+    al_draw_bitmap(bot_info[0], 0, 0, 0);
+}
+
+void Game_scene::keyboard_act(){
+    if(key_state[ALLEGRO_KEY_W])cur_controller[W_BUTTON] = controller[W_BUTTON][1];
+    else cur_controller[W_BUTTON] = controller[W_BUTTON][0];
+
+    if(key_state[ALLEGRO_KEY_A])cur_controller[A_BUTTON] = controller[A_BUTTON][1];
+    else cur_controller[A_BUTTON] = controller[A_BUTTON][0];
+
+    if(key_state[ALLEGRO_KEY_S])cur_controller[S_BUTTON] = controller[S_BUTTON][1];
+    else cur_controller[S_BUTTON] = controller[S_BUTTON][0];
+
+    if(key_state[ALLEGRO_KEY_D])cur_controller[D_BUTTON] = controller[D_BUTTON][1];
+    else cur_controller[D_BUTTON] = controller[D_BUTTON][0];
 }
 
 int Game_scene::mouse_act(int x, int y){
@@ -41,4 +76,10 @@ int Game_scene::mouse_act(int x, int y){
 Game_scene::~Game_scene(){
     for(int i = 0; i < map_num; i++)al_destroy_bitmap(maps[i]);
     for(int i = 0; i < 2; i++)al_destroy_bitmap(pause[i]);
+    for(int i = 0; i < 4; i++){
+        for(int j = 0;j < 2; j++)al_destroy_bitmap(controller[i][j]);
+        al_destroy_bitmap(cur_controller[i]);
+    }
+    al_destroy_bitmap(hp_bar[0]);
+    al_destroy_bitmap(bot_info[0]);
 }
